de3d8fe4f864319d38b35cb608691df7000eeaaa
723945465 <19871127ldLD@@>
2024-03-27 20:51:32 +0800

    111

--- MaintainTopicKeywords.py
+++ MaintainTopicKeywords.py
@@ -22,6 +22,63 @@ db_password= '19871127ldld'
 # {'keyword': '算法交易', 'weight': 10}
 # ]
 
+# xiaomi_car_keywords = [
+#     {'keyword': '小米汽车', 'weight': 10},
+#     {'keyword': 'SU7', 'weight': 10},
+#     {'keyword': '小米首款车型', 'weight': 10},
+#     {'keyword': '发布会', 'weight': 9},
+#     {'keyword': '定价', 'weight': 9},
+#     {'keyword': '雷军', 'weight': 9},
+#     {'keyword': 'V8S超级电机', 'weight': 8}
+# ]
+
+sorted_integrated_low_altitude_economy_keywords = [
+    {'keyword': '低空物流', 'weight': 10},
+    {'keyword': '低空经济', 'weight': 10},
+    {'keyword': 'eVTOL', 'weight': 10},
+    {'keyword': '低空医疗', 'weight': 10},
+    {'keyword': '低空航线', 'weight': 10},
+    {'keyword': '低空出行', 'weight': 10},
+    {'keyword': '垂直起降', 'weight': 8},
+    {'keyword': '空中交通', 'weight': 8},
+    {'keyword': '低空飞行器', 'weight': 8},
+    {'keyword': '智能飞行', 'weight': 8},
+]
+
+commercial_space_keywords = [
+    {'keyword': '运载火箭', 'weight': 9.5},
+    {'keyword': '卫星互联网', 'weight': 9.0},
+    {'keyword': '低轨', 'weight': 9.0},
+    {'keyword': '星链', 'weight': 9.5},
+    {'keyword': '可回收', 'weight': 8.5},
+    {'keyword': '星座', 'weight': 9.0},
+    {'keyword': '中国卫星通信集团', 'weight': 8},
+    {'keyword': '中国卫通', 'weight': 8},
+    {'keyword': '上海微小卫星', 'weight': 8},
+    {'keyword': 'SMIC', 'weight': 8},
+    {'keyword': '航天长峰', 'weight': 8},
+    {'keyword': '长峰航天', 'weight': 8},
+    {'keyword': '银河航天', 'weight': 8},
+    {'keyword': '银河互联网', 'weight': 8},
+    {'keyword': 'Galactic Network', 'weight': 8},
+    {'keyword': '蓝箭', 'weight': 8},
+    {'keyword': 'LandSpace', 'weight': 8},
+    {'keyword': '星际荣耀', 'weight': 8},
+    {'keyword': 'iSpace', 'weight': 8},
+    {'keyword': '零壹', 'weight': 8},
+    {'keyword': 'OneSpace', 'weight': 8},
+    {'keyword': '中科微星', 'weight': 7},
+    {'keyword': '天仪研究院', 'weight': 7},
+    {'keyword': '天仪', 'weight': 7},
+    {'keyword': 'Spacety', 'weight': 7},
+    {'keyword': '星网', 'weight': 9},
+    {'keyword': 'China Satellite Network', 'weight': 9},
+    {'keyword': '鸿雁星座', 'weight': 8},
+    {'keyword': '鸿雁', 'weight': 8},
+    {'keyword': '虹云', 'weight': 8},
+    {'keyword': '快舟', 'weight': 8},
+]
+
 def newInsertTopic(Topic_name, Topic_keywords):
     try:
         # 连接到MySQL数据库
@@ -46,3 +103,5 @@ def newInsertTopic(Topic_name, Topic_keywords):
         if connection.is_connected():
             cursor.close()
             connection.close()
+
+newInsertTopic('商业航天',commercial_space_keywords)
--- TopicKwordsAnalysis_Claude.py
+++ TopicKwordsAnalysis_Claude.py
@@ -22,8 +22,9 @@ def construct_topic_keywords(topic):
             # 构造关键词列表
             topic_keywords = []
             for row in rows:
-                keyword_dict = {'keyword': row[3], 'weight': int(row[4])}
-                topic_keywords.append(keyword_dict)
+                if row[3] is not None and row[4] is not None:
+                    keyword_dict = {'keyword': row[3], 'weight': int(row[4])}
+                    topic_keywords.append(keyword_dict)
 
             return topic_keywords
 
@@ -39,18 +40,14 @@ def construct_topic_keywords(topic):
             cursor.close()
             connection.close()
 
-def analyze_article(article_text, Topic_name):
+def analyze_one_article(article_text, Topic_keywords_dict_list, Topic_name):
     # 文本预处理
     article_text = article_text.lower()
     article_text = re.sub(r'[%s]' % re.escape(string.punctuation), '', article_text)
 
-    # 构造topic-keywords列表
-    Topic_keywords = construct_topic_keywords(Topic_name)
-    # print(Topic_keywords)
-
     # 统计关键词出现次数
     keyword_counts = {}
-    for keyword_dict in Topic_keywords:
+    for keyword_dict in Topic_keywords_dict_list:
         keyword = keyword_dict['keyword']
         count = article_text.count(keyword)
         if count > 0:
@@ -61,45 +58,151 @@ def analyze_article(article_text, Topic_name):
 
     # 计算文章与"量化交易"主题的匹配度分数
     total_weight = sum(
-        keyword_dict['weight'] * count for keyword_dict in Topic_keywords for keyword, count in
+        keyword_dict['weight'] * count for keyword_dict in Topic_keywords_dict_list for keyword, count in
         keyword_counts.items() if keyword_dict['keyword'] == keyword)
     article_length = len(article_text)
     match_score = total_weight / article_length if article_length > 0 else 0
 
-    match_Dict_toDB = {}
-    topic_key = Topic_name + "##" + str(match_score)
-    keywords_list = []
-    itor = 0
-    for temp_keyword in sorted_keywords:
-        if itor < 3:
-            itor = itor+1
-            temp_keyword_string = str(temp_keyword[0]) + "@@" + str(temp_keyword[1])
-            keywords_list.append(temp_keyword_string)
+    # eg。 {"量化##0.9294117647058824": ["量化@@2", "量化交易@@2", "算法交易@@1"]}
+    topic_match_condition_dict = {}
+    if(match_score > 0):
+        topic_key = Topic_name + "##" + str(match_score)
+        keywords_list = []
+        itor = 0
+        for temp_keyword in sorted_keywords:
+            if itor < 3:
+                itor = itor+1
+                temp_keyword_string = str(temp_keyword[0]) + "@@" + str(temp_keyword[1])
+                keywords_list.append(temp_keyword_string)
+
+        topic_match_condition_dict[topic_key] = keywords_list
+
+    return sorted_keywords, match_score, topic_match_condition_dict
 
 
-    match_Dict_toDB[topic_key] = keywords_list
+def analyze_recent_articles(Topic_name, is_Refresh_mode = False):
+    Topic_keywords_dict_list = construct_topic_keywords(Topic_name)
+    if len(Topic_keywords_dict_list) == 0:
+        print("Topic内没有关键词")
+        return
+    info_rows = []
+    try:
+        # 连接到MySQL数据库
+        connection = mysql.connector.connect(host=db_host, database=db_databasename, user=db_user, password=db_password)
+        if connection.is_connected():
+            cursor = connection.cursor()
+            if is_Refresh_mode == True:
+                query = """SELECT * FROM fetchtheworld.hismsg_info 
+                WHERE info_ready_for_analysis = 'yes' AND info_content is not null 
+                AND (info_bad_for_analysis != 'bad' OR info_bad_for_analysis IS NULL) 
+                AND create_time  >= '2024-03-21';"""
+            else:
+                query = f"""SELECT * FROM fetchtheworld.hismsg_info 
+                WHERE info_ready_for_analysis = 'yes' AND info_content is not null 
+                AND (info_bad_for_analysis != 'bad' OR info_bad_for_analysis IS NULL) 
+                AND create_time  >= '2024-03-21'
+                AND (info_analysis_status not like '%{Topic_name}%' OR info_analysis_status IS null);"""
+            cursor.execute(query)
+            info_rows = cursor.fetchall()
+    except Error as e:
+        print(f"Error while connecting to MySQL: {e}")
+        print(f"SQL STRING: {query}")
+        return []  # 发生错误时返回空
+    finally:
+        # 关闭数据库连接
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
 
-    return sorted_keywords, match_score,match_Dict_toDB
+    for temp_info in info_rows:
+        temp_info_id = temp_info[0]
+        temp_info_title = '' if temp_info[6] is None else temp_info[6]
+        temp_info_content = '' if temp_info[7] is None else temp_info[7]
+        temp_info_analysis_status_dict = {} if temp_info[11] is None else json.loads(temp_info[11])
+        temp_info_abstract = '' if temp_info[12] is None else temp_info[12]
+        temp_info_match_topic = [] if temp_info[13] is None else json.loads(temp_info[13])
+        match_condition_dict = {}
+        match_score = 0
+        matched_keyword_list = []
+        matched_keyword_list, match_score, match_condition_dict = analyze_one_article(temp_info_content, Topic_keywords_dict_list, Topic_name)
+
+        print(temp_info_id)
+        print(match_score)
+        print(match_condition_dict)
+
+
+        if('topic' in temp_info_analysis_status_dict.keys()):
+            TopicList = list(temp_info_analysis_status_dict['topic'])
+            if((Topic_name in TopicList) == False):
+                TopicList.append(Topic_name)
+                temp_info_analysis_status_dict['topic'] = TopicList
+        else:
+            temp_info_analysis_status_dict['topic'] = [Topic_name]
+
+        # 如果从数据库中读出来的info_match_topic是[]
+        if len(temp_info_match_topic) == 0:
+            if len(match_condition_dict) == 0:
+                temp_info_match_topic = []
+            else:
+                temp_info_match_topic = [match_condition_dict]
+
+        # 如果从数据库中读出来的info_match_topic已经有内容了，且match_condition_dict有新的匹配
+        # 则判断新的match_condition_dict是否和已有的记录存在分析同一个Topic的情况，
+        # 如果是同一个Topic，则用的新覆盖老的。
+        else:
+            if len(match_condition_dict) > 0:
+                has_topic_exsited = False
+                for match_item in temp_info_match_topic:
+                    first_key, first_value = next(iter(match_item.items()))
+                    if(str(first_key).find(Topic_name) >= 0):
+                        has_topic_exsited = True
+                if has_topic_exsited == False:
+                    temp_info_match_topic.append(match_condition_dict)
+
+        try:
+            # 连接到MySQL数据库
+            connection = mysql.connector.connect(host=db_host, database=db_databasename, user=db_user, password=db_password)
+            if connection.is_connected():
+                cursor = connection.cursor()
+                sql = ("UPDATE hismsg_info SET info_analysis_status = %s, info_match_topic = %s  WHERE id =  %s")
+                val = (json.dumps(temp_info_analysis_status_dict, ensure_ascii=False), json.dumps(temp_info_match_topic, ensure_ascii=False), temp_info_id)
+                cursor.execute(sql, val)
+                # 提交更改到数据库
+            connection.commit()
+
+        except Error as e:
+            print(f"Error while connecting to MySQL: {e}")
+            print(f"SQL STRING: {sql}")
+            return False  # 发生错误时返回False
+        finally:
+            # 关闭数据库连接
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
 
 
 
-
-
-article_text = "量化交易是一种利用数学模型和计算机算力进行交易决策的方法。它涉及算法交易、机器学习、高频交易等技术。量化交易的目标是通过对市场数据进行分析，发现交易机会并进行有效的交易。"
-
-matched_keywords, match_score, match_Dict_toDB = analyze_article(article_text,'量化')
-
-# print("匹配的关键词及次数(按匹配度降序排列):")
-# for keyword, count in matched_keywords:
-#     weight = next(keyword_dict['weight'] for keyword_dict in quantitative_trading_keywords if keyword_dict['keyword'] == keyword)
-#     print(f"{keyword}: {count} (权重: {weight})")
-# # print(matched_keywords)
-# print(f"\n与'量化交易'主题的匹配度分数: {match_score}")
+# article_text = "量化交易是一种利用数学模型和计算机算力进行交易决策的方法。它涉及算法交易、机器学习、高频交易等技术。量化交易的目标是通过对市场数据进行分析，发现交易机会并进行有效的交易。"
+# # 构造topic-keywords列表
+# Topic_keywords = construct_topic_keywords('量化')
+# # print(Topic_keywords)
+#
+# matched_keywords, match_score, match_Dict_toDB = analyze_one_article(article_text, Topic_keywords, '量化')
+#
+# # print("匹配的关键词及次数(按匹配度降序排列):")
+# # for keyword, count in matched_keywords:
+# #     weight = next(keyword_dict['weight'] for keyword_dict in quantitative_trading_keywords if keyword_dict['keyword'] == keyword)
+# #     print(f"{keyword}: {count} (权重: {weight})")
+# # # print(matched_keywords)
+# # print(f"\n与'量化交易'主题的匹配度分数: {match_score}")
+# # print(match_string_toDB)
+# match_string_toDB = json.dumps(match_Dict_toDB, ensure_ascii=False)
 # print(match_string_toDB)
-match_string_toDB = json.dumps(match_Dict_toDB, ensure_ascii=False)
-print(match_string_toDB)
-topic_key = list(json.loads(match_string_toDB).keys())[0]
-match_score = topic_key.split('##')[1]
-print(match_score)
+# topic_key = list(json.loads(match_string_toDB).keys())[0]
+# match_score = topic_key.split('##')[1]
+# print(match_score)
 
-#mark1
+analyze_recent_articles('量化')
+analyze_recent_articles('小米汽车')
+analyze_recent_articles('低空经济')
+analyze_recent_articles('商业航天')
